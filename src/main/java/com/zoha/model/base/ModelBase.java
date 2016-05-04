3cce122358cd5b22f510cea7f1947fbeb7364032
Amir Hooshangi <amirhoshangi@gmail.com>
2016-05-04 15:59:42 +0430

    model package: classes for basic authentications and tests.

---
+++ src/main/java/com/zoha/model/base/ModelBase.java
@@ -0,0 +1,7 @@
+package com.zoha.model.base;
+
+import java.io.Serializable;
+
+public class ModelBase implements Serializable {
+
+}
---
+++ src/main/java/com/zoha/model/factory/CerberusUserFactory.java
@@ -0,0 +1,19 @@
+package com.zoha.model.factory;
+
+
+import org.springframework.security.core.authority.AuthorityUtils;
+
+public class CerberusUserFactory {
+
+  /*public static CerberusUser create(User user) {
+  *//*  return new CerberusUser(
+      user.getId(),
+      user.getUsername(),
+      user.getPassword(),
+      user.getEmail(),
+      user.getLastPasswordReset(),
+      AuthorityUtils.commaSeparatedStringToAuthorityList(user.getAuthorities())
+    );*//*
+  }*/
+
+}
